0e96725d6c9b724a8e89bae287aa89a31e7754ca
narayanan8200 <narayanan8200@gmail.com>
2024-08-15 12:45:30 +0530

    Patient related changes

---
+++ patient/booking-complete.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/animations.css">  
+    <link rel="stylesheet" href="../css/main.css">  
+    <link rel="stylesheet" href="../css/admin.css">
+        
+    <title>Appointments</title>
+    <style>
+        .popup{
+            animation: transitionIn-Y-bottom 0.5s;
+        }
+        .sub-table{
+            animation: transitionIn-Y-bottom 0.5s;
+        }
+</style>
+<?php
+
+    //learn from w3schools.com
+
+    session_start();
+
+    if(isset($_SESSION["user"])){
+        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='p'){
+            header("location: ../login.php");
+        }else{
+            $useremail=$_SESSION["user"];
+        }
+
+    }else{
+        header("location: ../login.php");
+    }
+    
+
+    //import database
+    include("../connection.php");
+    $sqlmain= "select * from patient where pemail=?";
+    $stmt = $database->prepare($sqlmain);
+    $stmt->bind_param("s",$useremail);
+    $stmt->execute();
+    $userrow = $stmt->get_result();
+    $userfetch=$userrow->fetch_assoc();
+    $userid= $userfetch["pid"];
+    $username=$userfetch["pname"];
+
+    //To handle max session
+                                    if($_POST['apponum'] > $_POST["nop"]) {
+                                        
+                                        echo '
+            <div id="popup1" class="overlay">
+                    <div class="popup">
+                    <center>
+                    <br><br>
+                        <h2>Maximux Appointment Reached</h2>
+                        <a class="close" href="appointment.php">&times;</a>
+                        <div class="content"><br><br>
+                            
+                        </div>
+                        <div style="display: flex;justify-content: center;">
+                        
+                        <a href="doctors.php" class="non-style-link"><button  class="btn-primary btn"  style="display: flex;justify-content: center;align-items: center;margin:10px;padding:10px;"><font class="tn-in-text">&nbsp;&nbsp;Failed&nbsp;&nbsp;</font></button></a>
+                        <br><br><br><br>
+                        </div>
+                    </center>
+            </div>
+            </div>
+            ';
+                                    } else {
+
+    if($_POST){
+        if(isset($_POST["booknow"])){
+            $apponum=$_POST["apponum"];
+            $scheduleid=$_POST["scheduleid"];
+            $date=$_POST["date"];
+            $scheduleid=$_POST["scheduleid"];
+            $sql2="insert into appointment(pid,apponum,scheduleid,appodate) values ($userid,$apponum,$scheduleid,'$date')";
+            
+            $result= $database->query($sql2);
+            //echo $apponom;
+            header("location: appointment.php?action=booking-added&id=".$apponum."&titleget=none");
+
+        }
+    }
+}
+ ?>
+
+
+</body>
+</html>
